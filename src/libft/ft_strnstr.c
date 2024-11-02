fe1df7effcb78cc73b2a5ef7e81e53714c1a35f3
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-11-02 17:10:48 +0200

    libft: strnstr without test

---
+++ src/libft/ft_strnstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Xifeng <xifeng@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 16:55:33 by Xifeng            #+#    #+#             */
+/*   Updated: 2024/11/02 17:09:58 by Xifeng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (len == 0 || !*little)
+		return (char *)(big);
+    i = 0;
+	while (big[i] && i < len)
+	{
+		if (big[i] == little[0])
+		{
+			j = 0;
+			while (little[j] && i + j < len && little[j] == big[i + j])
+				++j;
+            if (!little[j])
+			    return (char *)(big + i);
+		}
+		++i;
+	}
+	return (0);
+}
