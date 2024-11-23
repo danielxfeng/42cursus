5e0b919aacd0a1ef8b6f33f714cf1fb878f8bcfa
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-11-23 19:17:48 +0000

    getnextline: restructured

--- src/ft_printf/ft_printf.c
+++ src/ft_printf/ft_printf.c
@@ -6,11 +6,11 @@
 /*   By: Xifeng <xifeng@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 19:49:44 by Xifeng            #+#    #+#             */
-/*   Updated: 2024/11/12 18:21:22 by Xifeng           ###   ########.fr       */
+/*   Updated: 2024/11/23 19:10:10 by Xifeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "ft_printf_helper.h"
 #include <stdarg.h>
 #include <unistd.h>
 
--- src/ft_printf/ft_printf_helper.h
+++ src/ft_printf/ft_printf_helper.h
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*   ft_printf_helper.h                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: Xifeng <xifeng@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/01 18:14:56 by Xifeng            #+#    #+#             */
-/*   Updated: 2024/11/23 19:09:58 by Xifeng           ###   ########.fr       */
+/*   Created: 2024/11/23 18:59:59 by Xifeng            #+#    #+#             */
+/*   Updated: 2024/11/23 19:01:16 by Xifeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf_helper.h"
+#ifndef FT_PRINTF_HELPER_H
+# define FT_PRINTF_HELPER_H
 
-void	ft_bzero(void *s, size_t n)
-{
-	ft_memset(s, '\0', n);
-}
+# include <unistd.h>
+
+int		print_char(char c);
+int		print_str(char *s);
+int		print_ptr(void *ptr);
+int		print_nbr(int n);
+int		print_hex(unsigned int n, int is_lowercase);
+int		print_unsigned_nbr(unsigned int n);
+void	ft_bzero(void *s, size_t n);
+void	*ft_memset(void *s, int c, size_t n);
+int		ft_strlen(const char *s);
+
+#endif
