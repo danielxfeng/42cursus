c5150df3a5927910ee5729e2f8e486144f1096f6
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-12-07 21:58:12 +0000

    pipex: fixed print

--- src/pipe_x/ast/ast_print.c
+++ src/pipe_x/ast/ast_print.c
@@ -6,7 +6,7 @@
 /*   By: Xifeng <xifeng@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 18:45:09 by Xifeng            #+#    #+#             */
-/*   Updated: 2024/12/07 20:28:47 by Xifeng           ###   ########.fr       */
+/*   Updated: 2024/12/07 21:07:12 by Xifeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ static void print_pipe_helper(t_ast_node *node)
     t_pipe_prop *prop;
 
     prop = (t_pipe_prop *)node->prop;
-    ft_printf("PIPE, fd0: %d, fd1: %d, pid: %d\n", 
+    ft_printf("-PIP, fd0: %d, fd1: %d, pid: %d\n", 
     prop->fds[0], prop->fds[1], prop->pid);
 }
 
@@ -28,7 +28,7 @@ static void print_cmd_helper(t_ast_node *node)
     int i;
 
     prop = (t_cmd_prop *)node->prop;
-    ft_printf("CMD, cmd: %s, args: ", prop->cmd);
+    ft_printf("-CMD, cmd: %s, args: ", prop->cmd);
     i = 0;
     while (prop->args[i])
         ft_printf("%s, ", prop->args[i++]);
@@ -40,7 +40,7 @@ static void print_red_helper(t_ast_node *node)
     t_red_prop *prop;
 
     prop = (t_red_prop *)node->prop;
-    ft_printf("RED, file: %s, fd: %d, is_in: %d, is_single: %d\n", 
+    ft_printf("-RED, file: %s, fd: %d, is_in: %d, is_single: %d\n", 
     prop->file_name, prop->fd, prop->is_in, prop->is_single);
 }
 
@@ -49,14 +49,11 @@ static void print_ast_helper(t_ast_node *node, int layer)
     int i;
 
     i = 0;
-    while (i++ < layer)
-        ft_printf("  ");
     if (!node)
-    {
-        ft_printf("NULL\n");
         return ;
-    }
     print_ast_helper(node->left, layer + 1);
+    while (i++ < layer)
+        ft_printf("   ");
     if (node->type == PIPE)
         print_pipe_helper(node);
     else if (node->type == CMD)
@@ -70,7 +67,7 @@ static void print_ast_helper(t_ast_node *node, int layer)
 void print_ast(t_ast *ast)
 {
     ft_printf("-----------------------AST Tree-----------------------\n");
-    if (!ast || !(ast->root))
+    if (!ast)
     {
         ft_printf("NULL\n");
         return ;
--- test/pipe_x/test_pipe_x.c
+++ test/pipe_x/test_pipe_x.c
@@ -77,20 +77,21 @@ void test_create_red_node(void)
     close_ast(&ast);
 }
 
+// here < ls -r 000 | >> here
 void test_create_multi_nodes_ast(void)
 {
     t_ast *ast = create_ast();
     ast->root = create_pipe_node(ast);
-    ast->root->left = create_cmd_node(ast, "ls -r 000");
-    ast->root->right = create_red_node(ast, "here", false, true);
-    ast->root->left->left = create_red_node(ast, "here", true, false);
+    ast->root->left = create_red_node(ast, "here", true, true);
+    ast->root->right = create_red_node(ast, "here", false, false);
+    ast->root->left->right = create_cmd_node(ast, "ls -r 000");
     TEST_ASSERT_EQUAL_INT(PIPE, ast->root->type);
     t_pipe_prop *prop = ast->root->prop;
     TEST_ASSERT_EQUAL_INT(-1, prop->fds[0]);
     TEST_ASSERT_EQUAL_INT(-1, prop->fds[1]);
     TEST_ASSERT_EQUAL_INT(0, prop->pid);
-    TEST_ASSERT_EQUAL_INT(CMD, ast->root->left->type);
-    t_cmd_prop *prop2 = ast->root->left->prop;
+    TEST_ASSERT_EQUAL_INT(CMD, ast->root->left->right->type);
+    t_cmd_prop *prop2 = ast->root->left->right->prop;
     TEST_ASSERT_EQUAL_STRING("ls", prop2->cmd);
     TEST_ASSERT_EQUAL_STRING("ls", prop2->args[0]);
     TEST_ASSERT_EQUAL_STRING("-r", prop2->args[1]);
@@ -100,12 +101,12 @@ void test_create_multi_nodes_ast(void)
     TEST_ASSERT_EQUAL_STRING("here", prop3->file_name);
     TEST_ASSERT_EQUAL_INT(-1, prop3->fd);
     TEST_ASSERT_EQUAL_INT(false, prop3->is_in);
-    TEST_ASSERT_EQUAL_INT(true, prop3->is_single);
-    t_red_prop *prop4 = ast->root->left->left->prop;
+    TEST_ASSERT_EQUAL_INT(false, prop3->is_single);
+    t_red_prop *prop4 = ast->root->left->prop;
     TEST_ASSERT_EQUAL_STRING("here", prop4->file_name);
     TEST_ASSERT_EQUAL_INT(-1, prop4->fd);
     TEST_ASSERT_EQUAL_INT(true, prop4->is_in);
-    TEST_ASSERT_EQUAL_INT(false, prop4->is_single);
+    TEST_ASSERT_EQUAL_INT(true, prop4->is_single);
     print_ast(ast);
     close_ast(&ast);
 }
