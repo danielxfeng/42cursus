8da12dd43a07a6ce84dc0cac6252dd4fad643c8f
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-12-11 11:36:45 +0200

    pipex: fixed test

--- test/pipe_x/test_pipe_x.c
+++ test/pipe_x/test_pipe_x.c
@@ -23,8 +23,9 @@ void	tearDown(void)
 
 void test_create_ast(void)
 {
-    char **path = calloc(1, sizeof(char *));
-    t_ast *ast = create_ast("", path);
+    char **envp = NULL;
+    char **path = parse_path(envp);
+    t_ast *ast = create_ast(envp, path);
     TEST_ASSERT_NULL(ast->root);
     TEST_ASSERT_EQUAL_CHAR_ARRAY(path, ast->path, 2);
     print_ast(ast);
@@ -33,11 +34,9 @@ void test_create_ast(void)
 
 void test_create_pipe_node(void)
 {
-    char **path = calloc(2, sizeof(char *));
-    path[0] = calloc(2, sizeof(char));
-    path[0][0] = 'a';
-    char enpv[100][100] = {'PATH=a'};
-    t_ast *ast = create_ast(enpv, path);
+    char *envp[] = {"PATH=a"};
+    char **path = parse_path(envp);
+    t_ast *ast = create_ast(envp, path);
     ast->root = create_pipe_node(ast);
     TEST_ASSERT_EQUAL_INT(PIPE, ast->root->type);
     t_pipe_prop *prop = ast->root->prop;
@@ -52,11 +51,9 @@ void test_create_pipe_node(void)
 
 void test_create_cmd_node(void)
 {
-    char **path = calloc(2, sizeof(char *));
-    path[0] = calloc(2, sizeof(char));
-    path[0][0] = 'a';
-    char enpv[100][100] = {'PATH=a'};
-    t_ast *ast = create_ast(enpv, path);
+    char *envp[] = {"PATH=a"};
+    char **path = parse_path(envp);
+    t_ast *ast = create_ast(envp, path);
     ast->root = create_cmd_node(ast, "ls -r 000");
     TEST_ASSERT_EQUAL_INT(CMD, ast->root->type);
     t_cmd_prop *prop = ast->root->prop;
@@ -70,11 +67,9 @@ void test_create_cmd_node(void)
 
 void test_create_red_node(void)
 {
-    char **path = calloc(2, sizeof(char *));
-    path[0] = calloc(2, sizeof(char));
-    path[0][0] = 'a';
-    char enpv[100][100] = {'PATH=a'};
-    t_ast *ast = create_ast(enpv, path);
+    char *envp[] = {"PATH=a"};
+    char **path = parse_path(envp);
+    t_ast *ast = create_ast(envp, path);
     ast->root = create_red_node(ast, "here", true, true);
     TEST_ASSERT_EQUAL_INT(RED, ast->root->type);
     t_red_prop *prop = ast->root->prop;
@@ -85,10 +80,8 @@ void test_create_red_node(void)
     TEST_ASSERT_EQUAL_CHAR_ARRAY(path, ast->path, 2);
     print_ast(ast);
     close_ast(&ast);
-    path = calloc(2, sizeof(char *));
-    path[0] = calloc(2, sizeof(char));
-    path[0][0] = 'a';
-    ast = create_ast(enpv, path);
+    path = parse_path(envp);
+    ast = create_ast(envp, path);
     ast->root = create_red_node(ast, "here", false, false);
     prop = ast->root->prop;
     TEST_ASSERT_EQUAL_INT(false, prop->is_in);
@@ -100,11 +93,9 @@ void test_create_red_node(void)
 // here < ls -r 000 | >> here
 void test_create_multi_nodes_ast(void)
 {
-    char **path = calloc(2, sizeof(char *));
-    path[0] = calloc(2, sizeof(char));
-    path[0][0] = 'a';
-    char enpv[100][100] = {'PATH=a'};
-    t_ast *ast = create_ast(enpv, path);
+    char *envp[] = {"PATH=a"};
+    char **path = parse_path(envp);
+    t_ast *ast = create_ast(envp, path);
     ast->root = create_pipe_node(ast);
     ast->root->left = create_red_node(ast, "here", true, true);
     ast->root->right = create_red_node(ast, "here", false, false);
