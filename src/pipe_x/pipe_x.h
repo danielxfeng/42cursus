513165c9a79a60a40fda628c21b87973b4214cc8
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2024-12-14 11:11:06 +0200

    pipex: fixed the err messages.

--- src/pipe_x/pipe_x.h
+++ src/pipe_x/pipe_x.h
@@ -6,7 +6,7 @@
 /*   By: Xifeng <xifeng@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/03 20:24:37 by Xifeng            #+#    #+#             */
-/*   Updated: 2024/12/14 11:07:20 by Xifeng           ###   ########.fr       */
+/*   Updated: 2024/12/14 11:10:50 by Xifeng           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,11 @@
 # include <sys/types.h>
 # include <unistd.h>
 
-# define MALLOC_ERR ": malloc() failed"
+# define MALLOC_ERR "malloc() failed"
 # define FORK_ERR "fork() error"
 # define PIPE_ERR "pipe() creation failed"
 # define PERMISSION_ERR "Permission denied"
-# define FILE_ERR ": No such file or directory"
+# define FILE_ERR "No such file or directory"
 # define DIRECTORY_ERR "Is a directory"
 # define DUP_ERR "dup2() error"
 # define ORIGIN_DUP_ERR "dup() error"
