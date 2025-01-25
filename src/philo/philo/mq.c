53817b28f69c9f5871df52f9ff9a326aac435aa8
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-01-25 13:11:43 +0000

    philo: added the message queue.

---
+++ src/philo/philo/mq.c
@@ -0,0 +1,164 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mq_factory.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Xifeng <xifeng@student.hive.fi>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/25 10:51:47 by Xifeng            #+#    #+#             */
+/*   Updated: 2025/01/25 13:08:23 by Xifeng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+#include <stdlib.h>
+
+// @brief Destructor of mq.
+// Call it ONLY when holding the lock of mq or the lock is not inited.
+//
+// @param mq: pointer to the message queue.
+// @param has_lock: if the lock is initailized.
+// @return NULL
+t_mq *close_mq(t_mq **mq, bool has_lock)
+{
+    if (mq && *mq)
+    {
+        (*mq)->is_closed = true;
+        if ((*mq)->events)
+        {
+            free((*mq)->events);
+            (*mq)->events = NULL;
+        }
+        if ((*mq)->philo_ids)
+        {
+            free((*mq)->philo_ids);
+            (*mq)->philo_ids = NULL;
+        }
+        if ((*mq)->timestamps)
+        {
+            free((*mq)->timestamps);
+            (*mq)->timestamps = NULL;
+        }
+        if (has_lock)
+            pthread_mutex_destroy(&((*mq)->lock));
+        free(*mq);
+        *mq = NULL;
+    }
+    return (NULL);
+}
+
+// @brief The constructor of a fixed length message queue.
+//
+// @param capacity: capacity of the mq. Note for a fixed length mq,
+// the data will be overwrited when the mq is full.
+// @return the pointer to the message queue. will return NULL on error.
+t_mq *create_mq(int capacity) {
+    t_mq *mq;
+    
+    mq = malloc(sizeof(t_mq));
+    if (!mq)
+        return (NULL);
+    if (pthread_mutex_init(&(mq->lock), NULL))
+        return (close_mq(&mq, false));
+    mq->capacity = capacity;
+    mq->write = 0;
+    mq->read = 0;
+    mq->is_closed = false;
+    mq->timestamps = malloc(capacity * sizeof(int));
+    if (!mq->timestamps)
+        return (close_mq(&mq, true));
+    mq->events = malloc(capacity * sizeof(int));
+    if (!mq->events)
+        return (close_mq(&mq, true));
+    mq->philo_ids = malloc(capacity * sizeof(int));
+    if (!mq->philo_ids)
+        return (close_mq(&mq, true));
+    return (mq);
+}
+
+// @brief Returns the size of a message queue.
+// Caution: Be sure you have a valid mq, and hold the lock before call it.
+//
+// @param mq: the pointer of message queue, should not be NULL.
+static int size_of_mq(t_mq *mq)
+{
+    int diff;
+    
+    diff = mq->write - mq->read;
+    if (diff >= 0)
+        return (diff);
+    return (mq->capacity + diff);
+}
+
+// @brief Send a message to message queue (thread safe).
+//
+// @param mq: the pointer to message queue.
+// @param ts: the timestamp of a message.
+// @param id: the philo's id of a message.
+// @param event: the event of a message.
+// @return false on error or mq is closed, otherwise returns true.
+bool send_message(t_mq *mq, int ts, int id, int event)
+{
+    if (!mq || pthread_mutex_lock(&(mq->lock)) != 0)
+        return (false);
+    if (mq->is_closed)
+    {
+        pthread_mutex_unlock(&(mq->lock));
+        return (false);        
+    }
+    mq->write = (mq->write + 1) % mq->capacity;
+    mq->timestamps[mq->write] = ts;
+    mq->events[mq->write] = event;
+    mq->philo_ids[mq->write] = id;
+    pthread_mutex_unlock(&(mq->lock));
+    return (true);
+}
+
+// @brief A helper function for print_message.
+// Please Note the caller should hold a lock.
+//
+// @param mq: the pointer to mq.
+// @return: false if there is a "Dead" philo, otherwise returns true.
+static bool print_message_helper(t_mq *mq)
+{
+    int i;
+    int idx;
+    
+    i = 0;
+    while (i < size_of_mq(mq)) 
+    {
+        idx = (i++ + mq->read) % mq->capacity;
+        if (mq->events[idx] == GET_FORK)
+            printf("%d %d %s", mq->timestamps[idx], mq->philo_ids[idx], "has taken a fork");
+        else if (mq->events[idx] == EATING)
+            printf("%d %d %s", mq->timestamps[idx], mq->philo_ids[idx], "is eating"); 
+        else if (mq->events[idx] == SLEEPING)
+            printf("%d %d %s", mq->timestamps[idx], mq->philo_ids[idx], "is sleeping");  
+        else if (mq->events[idx] == THINKING)
+            printf("%d %d %s", mq->timestamps[idx], mq->philo_ids[idx], "is thinking");  
+        else if (mq->events[idx] == DEAD)
+        {
+            printf("%d %d %s", mq->timestamps[idx], mq->philo_ids[idx], "is died");   
+            return (false);
+        }
+    }
+    return (true);
+}
+
+// @brief Print the messages from message queue (thread safe).
+//
+// @param mq: the pointer to message queue.
+// @return false on error or mq is closed, otherwise returns true.
+bool print_message(t_mq *mq)
+{   
+    if (!mq || pthread_mutex_lock(&(mq->lock)) != 0)
+        return (false);
+    if (mq->is_closed || !print_message_helper(mq))
+    {
+        pthread_mutex_unlock(&(mq->lock));
+        return (false);
+    }
+    mq->read = (size_of_mq(mq) + mq->read) % mq->capacity;    
+    pthread_mutex_unlock(&(mq->lock));
+    return (true);
+}
