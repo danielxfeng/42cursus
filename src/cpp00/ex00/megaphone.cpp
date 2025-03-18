edbb71b3a1ec59a19d4ba3e6ec2b05dbb73c38e2
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-03-18 11:44:35 +0200

    cpp00-ex00

---
+++ src/cpp00/ex00/megaphone.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+    else
+    {
+        for (int i = 1; i < argc; ++i)
+        {
+            int j = 0;
+            while (argv[i][j])
+            {
+                std::cout << (char)std::toupper(argv[i][j]);
+                ++j;
+            }
+        }
+    }
+    std::cout << std:: endl;
+    return EXIT_SUCCESS;
+}
