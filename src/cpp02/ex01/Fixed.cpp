d575bdb52359efca6aa014ff9279fa3669bda29d
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-03-20 21:17:56 +0200

    cpp02ex01

---
+++ src/cpp02/ex01/Fixed.cpp
@@ -0,0 +1,52 @@
+# include "Fixed.hpp"
+# include <iostream>
+
+Fixed::Fixed() : value_(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const int value) {
+    std::cout << "Int constructor called" << std::endl;
+    value_ = value << fractionalBits_;
+}
+
+Fixed::Fixed(const float value) {
+    std::cout << "Float constructor called" << std::endl;
+    value_ = (int)(value * (1 << fractionalBits_));
+}
+
+Fixed::Fixed(const Fixed &o) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = o;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &o) {
+    std::cout << "Copy assignment operator called" << std::endl;
+    value_ = o.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits(void) const {
+    return value_;
+}
+
+void Fixed::setRawBits(int const value) {
+    value_ = value;
+}
+
+float Fixed::toFloat( void ) const {
+    return (float)value_ / (1 << fractionalBits_);
+}
+
+int Fixed::toInt( void ) const {
+    return value_ >> fractionalBits_;
+}
+
+std::ostream &operator<<(std::ostream &os, Fixed const &n) {
+    os << n.toFloat();
+    return os;
+}
--- src/cpp02/ex01/Fixed.hpp
+++ src/cpp02/ex01/Fixed.hpp
@@ -3,18 +3,27 @@
 # ifndef FIXED_HPP
 # define FIXED_HPP
 
+# include <iostream>
+
 class Fixed {
     public:
         Fixed();
+        Fixed(const int value);
+        Fixed(const float value);
+
         Fixed(const Fixed &o);
         ~Fixed();
         Fixed &operator=(const Fixed &o);
         int getRawBits(void) const;
         void setRawBits(int const value);
+        float toFloat( void ) const;
+        int toInt( void ) const;
 
     private:
         int value_;
         static const int fractionalBits_ = 8;
 };
 
-# endif // FIXED_HPP
+std::ostream &operator<<(std::ostream &os, Fixed const &n);
+
+# endif
