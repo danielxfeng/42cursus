72b9d8e709ef6646b2d2045abe4b0e382d13d72f
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-05-15 13:44:45 +0300

    fxied cpp05ex03

--- src/cpp05/ex03/Intern.cpp
+++ src/cpp05/ex03/Intern.cpp
@@ -8,7 +8,8 @@ Intern::Intern(const Intern &o) { *this = o; }
 Intern &Intern::operator=(const Intern &o)
 {
     if (this != &o)
-        *this = o;
+    {
+    }
     return *this;
 }
 Intern::~Intern() {}
@@ -21,25 +22,23 @@ AForm *Intern::makeForm(const std::string name, const std::string target)
         "ShrubberyCreationForm",
     };
 
-    size_t idx = 4;
+    typedef AForm *(*FormFactory)(const std::string &);
+
+    FormFactory factory[] = {
+        [](const std::string &t) -> AForm *
+        { return new PresidentialPardonForm(t); },
+        [](const std::string &t) -> AForm *
+        { return new RobotomyRequestForm(t); },
+        [](const std::string &t) -> AForm *
+        { return new ShrubberyCreationForm(t); },
+    };
+
     for (size_t i = 0; i < 3; ++i)
     {
         if (names[i] == name)
         {
-            idx = i;
-            break;
+            return factory[i](target);
         }
-    };
-
-    switch (idx)
-    {
-    case 0:
-        return new PresidentialPardonForm(target);
-    case 1:
-        return new RobotomyRequestForm(target);
-    case 2:
-        return new ShrubberyCreationForm(target);
-    default:
-        throw std::out_of_range("Illegal form name.");
     }
+    throw std::out_of_range("Illegal form name.");
 }
