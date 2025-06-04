dbf13500ddb9147fcaf4337e1189ca71c060491f
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-06-04 13:01:20 +0300

    fixed cpp07, added const member.

--- src/cpp07/ex01/iter.hpp
+++ src/cpp07/ex01/iter.hpp
@@ -2,7 +2,14 @@
 #include <cstddef>
 
 template <typename T>
-void iter(T *ptr, size_t len, void(*func)(T&))
+void iter(T *ptr, size_t len, void (*func)(T &))
+{
+    for (size_t i = 0; i < len; ++i)
+        func(ptr[i]);
+}
+
+template <typename T>
+void iter(T *ptr, size_t len, void (*func)(const T &))
 {
     for (size_t i = 0; i < len; ++i)
         func(ptr[i]);
