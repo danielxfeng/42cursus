0e61c0ed9d10af060abc3143db2eec79b2ad2a74
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-06-17 17:02:29 +0300

    test settings

--- test/cpp09/ex02/test.cpp
+++ test/cpp09/ex02/test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "PmergeMe.hpp"
+#include "algorithm"
 
 TEST(PairwiseComparator, Test) {
     std::vector<int> vec = {4, 7, 9, 8, 6};
@@ -20,4 +21,18 @@ TEST(mergeInsertionSortFirstPart, Test) {
     std::vector<int> out = {6, 15, 8, 16, 2, 11, 0, 17, 9, 18, 14, 19, 3, 10, 1, 21, 5, 12, 4, 20, 7, 13};
     mergeInsertionSort(vec, 1, false);
     EXPECT_EQ(vec, out);
+}
+
+TEST(mergeInsertionSortFull, Test) {
+    std::vector<int> vec = {11, 2, 17, 0, 16, 8, 6, 15, 10, 3, 21, 1, 18, 9, 14, 19, 12, 5, 4, 20, 13, 7};
+    auto span = std::span<int>(vec);
+    std::vector<int> out = vec;
+    std::sort(out.begin(), out.end());
+    mergeInsertionSort(vec, 1, true);
+    EXPECT_EQ(vec, out);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
