4eab6f51b2006ec97ab7df838fe4902ff8c32fa1
Xin Feng <126309503+danielxfeng@users.noreply.github.com>
2025-10-21 12:31:03 +0300

    feat/inception: it works now

--- src/inception/srcs/requirements/mariadb/tools/start.sh
+++ src/inception/srcs/requirements/mariadb/tools/start.sh
@@ -3,7 +3,8 @@ set -e
 
 chown -R mysql:mysql /var/lib/mysql
 
-if [ ! -d /var/lib/mysql/mysql ]; then
+
+if [ ! -f /var/lib/mysql/mysql ]; then
     echo "MariaDB initializing database..."
     mysql_install_db --user=mysql --ldata=/var/lib/mysql > /dev/null
 
@@ -11,7 +12,6 @@ if [ ! -d /var/lib/mysql/mysql ]; then
 
     cat <<EOF | mysqld --bootstrap --user=mysql --skip-grant-tables=OFF
 USE mysql;
-FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
 CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
